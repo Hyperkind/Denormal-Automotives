f0dbc386f219117075eeb0d465456f942f045c85
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-14 19:48:30 -1000

    Denormal Cars 6 is working

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -1,6 +1,12 @@
 \c denormal_cars;
 
--- Query to list all make_title values in the car_models table
+-- 5 Query to list all make_title values in the car_models table
 -- without any duplicate rows (71)
-SELECT DISTINCT make_title
-  FROM car_models;
+-- SELECT DISTINCT make_title
+--   FROM car_models;
+
+-- 6 Query to list all model_title values where the make_code is 'VOLKS'
+-- without any duplicate rows (27)
+SELECT DISTINCT model_title
+  FROM car_models
+WHERE make_code = 'VOLKS';
