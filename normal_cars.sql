24a83d8ab536afa60e634dc5e3287993fa9fb289
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-16 12:27:31 -1000

    everything is normalized

--- normal_cars.sql
+++ normal_cars.sql
@@ -1,47 +1,38 @@
 \c denormal_cars;
 
 -- Drops table Make if exists
-DROP TABLE IF EXISTS make;
+DROP TABLE IF EXISTS make CASCADE;
 
 -- Creates table Make
 CREATE TABLE make(
   id serial PRIMARY KEY NOT NULL,
-  make_code character varying(20) default NULL,
-  make_title character varying(20) default NULL
+  make_code character varying(125) NOT NULL,
+  make_title character varying(125) NOT NULL
 );
 
 -- Drops table model if exists
-DROP TABLE IF EXISTS model;
+DROP TABLE IF EXISTS model CASCADE;
 
 -- Creates table model
 CREATE TABLE model(
   id serial PRIMARY KEY NOT NULL,
-  model_code character varying(50) default NULL,
-  model_title character varying(50) default NULL
-);
-
--- Drops table year if exists
-DROP TABLE IF EXISTS year;
-
--- Creates table year
-CREATE TABLE year(
-  id serial PRIMARY KEY NOT NULL,
-  year character varying(4) default NULL
+  model_code character varying(125) NOT NULL,
+  model_title character varying(125) NOT NULL,
+  year integer NOT NULL,
+  make_id integer REFERENCES make(id)
 );
 
 -- Drops table cars if exists
--- DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS normal_cars CASCADE;
 
--- Creates table cars
--- CREATE TABLE cars(
---   id serial PRIMARY KEY NOT NULL,
---   make character varying(40) default NULL,
---   model character varying(40) default NULL,
---   year character varying(40) default NULL,
---   make_id integer REFERENCES make(id),
---   model_id integer REFERENCES model(id),
---   year_id integer REFERENCES year(id)
--- );
+-- Creates table normal_cars
+CREATE TABLE normal_cars(
+  id serial PRIMARY KEY NOT NULL,
+  make character varying(125) default NULL,
+  model character varying(125) default NULL,
+  make_id integer REFERENCES make(id),
+  model_id integer REFERENCES model(id)
+);
 
 -- Inserts into make table
 INSERT INTO make (make_code, make_title)
@@ -49,11 +40,13 @@ INSERT INTO make (make_code, make_title)
   FROM car_models;
 
 -- Inserts into model table
-INSERT INTO model (model_code, model_title)
-  SELECT DISTINCT model_code, model_title
-  FROM car_models;
+INSERT INTO model (model_code, model_title, year, make_id)
+  SELECT model_code, model_title, year, make.id
+  FROM car_models
+    INNER JOIN make ON (make.make_code = car_models.make_code);
 
--- Inserts years into year table
-INSERT INTO year
-  SELECT DISTINCT year
-  FROM car_models;
+-- Inserts into normal_cars
+INSERT INTO normal_cars (make, model)
+  SELECT make.id, model.id
+  FROM model
+    INNER JOIN make ON (make.id = model.make_id);
